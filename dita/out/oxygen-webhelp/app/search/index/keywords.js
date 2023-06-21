2301d8b7594c422e4d1dec2360395e1150182f89
brahnavan <brahnavan@brahnavans-MacBook-Pro.local>
2023-06-21 11:35:36 -0400

    #26 : Chnages to More mock class elements - output updated

--- dita/out/oxygen-webhelp/app/search/index/keywords.js
+++ dita/out/oxygen-webhelp/app/search/index/keywords.js
@@ -1,4 +1,4 @@
-var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"Regions",p:["p1"]},{w:"Europe",p:["p2"]},{w:"France",p:["p3"]},{w:"Unit_a",p:["p4"]},{w:"Links",p:["p5"]},{w:"d",p:["p5"]},{w:"Germany",p:["p6"]},{w:"Unit",p:["p7"]},{w:"a",p:["p7"]}];
+var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"Regions",p:["p1"]},{w:"Europe",p:["p2"]},{w:"France",p:["p3"]},{w:"Unit_a",p:["p4"]},{w:"Links",p:["p5"]},{w:"d",p:["p5"]},{w:"Spain",p:["p6"]},{w:"Unit_db",p:["p7"]}];
 var ph={};
 ph["p0"]=[0, 1, 2];
 ph["p1"]=[3];
@@ -7,7 +7,7 @@ ph["p3"]=[5];
 ph["p4"]=[6];
 ph["p5"]=[7, 8];
 ph["p6"]=[9];
-ph["p7"]=[10, 11];
+ph["p7"]=[10];
 var keywordsInfo = {
     keywords: keywords,
     ph: ph
--- dita/out/oxygen-webhelp/app/search/index/link-to-parent.js
+++ dita/out/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -1,2 +1,2 @@
 /*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
-var linkToParent = {8:6,1:-1,3:2,4:3,6:2,2:1,5:3,7:6};
+var linkToParent = {6:2,4:3,7:6,2:1,3:2,5:3,1:-1};
