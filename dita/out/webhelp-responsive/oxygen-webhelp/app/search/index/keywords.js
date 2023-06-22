ecb7667f26aac76da7784c9710f9db62578bf034
brahnavan <brahnavan@brahnavans-MacBook-Pro.local>
2023-06-22 13:56:14 -0400

    Change css to show table border

---
+++ dita/out/webhelp-responsive/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,14 @@
+define(function() {var keywords=[{w:"Regions",p:["p0"]},{w:"Europe",p:["p1"]},{w:"France",p:["p2"]},{w:"Unit_a",p:["p3"]},{w:"Unit_d",p:["p4"]},{w:"Spain",p:["p5"]},{w:"Unit_db",p:["p6"]}];
+var ph={};
+ph["p0"]=[0];
+ph["p1"]=[1];
+ph["p2"]=[2];
+ph["p3"]=[3];
+ph["p4"]=[4];
+ph["p5"]=[5];
+ph["p6"]=[6];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});
