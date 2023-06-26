6c76726537619af4fc24eb0429e60087a3eeec9e
brahnavan <brahnavan@brahnavans-MacBook-Pro.local>
2023-06-26 12:24:28 -0400

    Changes to correct id and remove un wanted things

--- dita/out/oxygen-webhelp/app/nav-links/json/france-id-d223e26.js
+++
@@ -1,1 +0,0 @@
-oxy_webhelp_navlinks = {"france-id-d223e26":{"topics":[{"title":"***","href":"europe\/france\/unit_a.html","menu":{"hasChildren":false},"tocID":"tocId-d223e34","topics":[]},{"title":"***","href":"europe\/france\/unit_d_star.html","menu":{"hasChildren":false},"tocID":"tocId-d223e37","topics":[]}]}};
