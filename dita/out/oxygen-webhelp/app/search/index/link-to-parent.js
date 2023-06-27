daead3e6e58cf56423838806f712b09e77a7f8bf
brahnavank <brahnavan@gmail.com>
2023-06-27 08:54:08 -0400

    Newly generated output

--- dita/out/oxygen-webhelp/app/search/index/link-to-parent.js
+++ dita/out/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -1,2 +1,2 @@
 /*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
-var linkToParent = {7:6,1:8,4:3,3:1,5:3,6:1,8:-1};
+var linkToParent = {4:3,7:6,3:1,1:8,8:-1,5:3,6:1};
