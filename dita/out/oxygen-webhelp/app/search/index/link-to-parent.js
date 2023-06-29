1cc4fce4c6ba726413b20311dd64dc7b74417464
brahnavank <brahnavan@gmail.com>
2023-06-29 08:50:17 -0400

    Add back files

--- dita/out/oxygen-webhelp/app/search/index/link-to-parent.js
+++ dita/out/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -1,4 +1,4 @@
 /*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
 define(function () {
-return {6:-1,8:-1,14:-1,12:-1,4:-1};
+return {13:-1,0:13,11:9,3:0,5:3,4:-1,12:-1,7:3,9:0,8:-1,6:-1};
 });
