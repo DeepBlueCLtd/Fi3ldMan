b009e4c4a549a4a598464f89a3a5e468ac888123
brahnavank <brahnavan@gmail.com>
2023-06-29 08:27:46 -0400

    Remove unwanted diatmaps

--- dita/out/oxygen-webhelp/app/search/index/link-to-parent.js
+++ dita/out/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -1,2 +1,4 @@
 /*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
-var linkToParent = {1:14,8:4,9:-1,5:-1,12:10,14:-1,4:1,10:1,13:-1,6:4,7:-1};
+define(function () {
+return {6:-1,8:-1,14:-1,12:-1,4:-1};
+});
