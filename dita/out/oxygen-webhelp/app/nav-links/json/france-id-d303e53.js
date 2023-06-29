8c3b25095b22dc6701b423dbc99d95ac603b2a95
Ian Mayo <ian@planetmayo.com>
2023-06-29 10:02:55 +0100

    update webhelp

---
+++ dita/out/oxygen-webhelp/app/nav-links/json/france-id-d303e53.js
@@ -0,0 +1,1 @@
+oxy_webhelp_navlinks = {"france-id-d303e53":{"topics":[{"title":"Unit_db","href":"regions\/europe\/spain\/unit_db.html","attributes":{"data-id":"unit_db"},"menu":{"hasChildren":false},"tocID":"unit_db-d303e61","topics":[]}]}};
--- dita/out/oxygen-webhelp/app/search/index/link-to-parent.js
+++ dita/out/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -1,4 +1,2 @@
 /*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
-define(function () {
-return {13:-1,1:14,12:10,14:-1,4:1,7:-1,6:4,5:-1,8:4,10:1,9:-1};
-});
+var linkToParent = {12:10,4:1,7:-1,8:4,9:-1,1:14,13:-1,14:-1,5:-1,10:1,6:4};
