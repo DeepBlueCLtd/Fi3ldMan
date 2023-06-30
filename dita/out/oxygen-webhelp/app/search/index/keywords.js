4f4ff4bef530924e6ad5bd544d12c4543bd102d6
Ian Mayo <ian@planetmayo.com>
2023-06-30 11:48:52 +0100

    update webhelp

--- dita/out/oxygen-webhelp/app/search/index/keywords.js
+++ dita/out/oxygen-webhelp/app/search/index/keywords.js
@@ -1,4 +1,4 @@
-define(function() {var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"Europe",p:["p1"]},{w:"French",p:["p2","p3"]},{w:"Cooling",p:["p2"]},{w:"Units",p:["p2","p3"]},{w:"Power",p:["p3"]},{w:"France",p:["p4"]},{w:"Unit_a",p:["p5"]},{w:"Unit_d",p:["p6"]},{w:"Spain",p:["p7","p8"]},{w:"Examples",p:["p8"]},{w:"of",p:["p8"]},{w:"Propulsion",p:["p8"]},{w:"in",p:["p8"]},{w:"Unit_db",p:["p9"]},{w:"Regions",p:["p10"]}];
+var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"Europe",p:["p1"]},{w:"French",p:["p2","p3"]},{w:"Cooling",p:["p2"]},{w:"Units",p:["p2","p3"]},{w:"Power",p:["p3"]},{w:"France",p:["p4"]},{w:"Unit_a",p:["p5"]},{w:"Unit_d",p:["p6"]},{w:"Spain",p:["p7","p8"]},{w:"Examples",p:["p8"]},{w:"of",p:["p8"]},{w:"Propulsion",p:["p8"]},{w:"in",p:["p8"]},{w:"Unit_db",p:["p9"]},{w:"Regions",p:["p10"]}];
 var ph={};
 ph["p0"]=[0, 1, 2];
 ph["p1"]=[3];
@@ -11,8 +11,7 @@ ph["p7"]=[11];
 ph["p8"]=[12, 13, 14, 15, 11];
 ph["p9"]=[16];
 ph["p10"]=[17];
-     return {
-         keywords: keywords,
-         ph: ph
-     }
-});
+var keywordsInfo = {
+    keywords: keywords,
+    ph: ph
+}
