9ae6ca66118b87e4e3a68e7693312d590ec4c076
Ian Mayo <ian@planetmayo.com>
2023-08-24 10:12:41 +0100

    Update generated content

--- dita/out/oxygen-webhelp/app/search/index/link-to-parent.js
+++ dita/out/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -1,2 +1,2 @@
 /*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
-var linkToParent = {40:-1,51:-1,32:-1,36:-1,34:-1};
+var linkToParent = {40:-1,51:-1,36:-1,32:-1,34:-1};
