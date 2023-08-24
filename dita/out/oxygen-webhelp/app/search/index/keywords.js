6112ca8499cf883f14afd3f62df4c4a582f08a53
Ian Mayo <ian@planetmayo.com>
2023-08-24 10:08:29 +0100

    update published content

--- dita/out/oxygen-webhelp/app/search/index/keywords.js
+++ dita/out/oxygen-webhelp/app/search/index/keywords.js
@@ -1,4 +1,4 @@
-var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"Europe",p:["p1"]},{w:"French",p:["p2","p3"]},{w:"Cooling",p:["p2","p11","p20"]},{w:"Units",p:["p2","p3","p11","p12","p20","p21"]},{w:"Power",p:["p3","p12","p21"]},{w:"France",p:["p4"]},{w:"Unit_a",p:["p5"]},{w:"Unit_d",p:["p6"]},{w:"Spain",p:["p7","p8"]},{w:"Examples",p:["p8"]},{w:"of",p:["p8"]},{w:"Propulsion",p:["p8"]},{w:"in",p:["p8"]},{w:"Unit_db",p:["p9"]},{w:"Regions",p:["p10"]},{w:"British",p:["p11","p12"]},{w:"Hybrid",p:["p11","p12","p14","p18","p20","p21"]},{w:"Britain",p:["p13","p14","p15"]},{w:"Legacy",p:["p15","p19"]},{w:"Unit_legacy_a",p:["p16"]},{w:"Unit_legacy_dstar",p:["p17"]},{w:"Wales",p:["p18","p19","p22"]},{w:"Welsh",p:["p20","p21"]}];
+var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"Europe",p:["p1"]},{w:"French",p:["p2","p3"]},{w:"Cooling",p:["p2","p11","p20"]},{w:"Units",p:["p2","p3","p11","p12","p20","p21"]},{w:"Power",p:["p3","p12","p21"]},{w:"France",p:["p4"]},{w:"Unit_a",p:["p5"]},{w:"Unit_d",p:["p6"]},{w:"Spain",p:["p7","p8"]},{w:"Examples",p:["p8"]},{w:"of",p:["p8"]},{w:"Propulsion",p:["p8"]},{w:"in",p:["p8"]},{w:"Unit_db",p:["p9"]},{w:"Regions",p:["p10"]},{w:"British",p:["p11","p12"]},{w:"Hybrid",p:["p11","p12","p14","p18","p20","p21"]},{w:"Britain",p:["p13","p14","p15"]},{w:"Legacy",p:["p15","p19"]},{w:"Unit_legacy_a",p:["p16"]},{w:"Unit_legacy_dstar",p:["p17"]},{w:"Wales",p:["p18","p19","p22"]},{w:"Welsh",p:["p20","p21"]},{w:"Africa",p:["p23"]},{w:"Iceland",p:["p24"]}];
 var ph={};
 ph["p0"]=[0, 1, 2];
 ph["p1"]=[3];
@@ -11,11 +11,8 @@ ph["p7"]=[11];
 ph["p8"]=[12, 13, 14, 15, 11];
 ph["p9"]=[16];
 ph["p10"]=[17];
-ph["p21"]=[25, 19, 7, 6];
-ph["p20"]=[25, 19, 5, 6];
 ph["p12"]=[18, 19, 7, 6];
 ph["p11"]=[18, 19, 5, 6];
-ph["p22"]=[24];
 ph["p14"]=[20, 19];
 ph["p13"]=[20];
 ph["p16"]=[22];
@@ -23,6 +20,11 @@ ph["p15"]=[20, 21];
 ph["p18"]=[24, 19];
 ph["p17"]=[23];
 ph["p19"]=[24, 21];
+ph["p21"]=[25, 19, 7, 6];
+ph["p20"]=[25, 19, 5, 6];
+ph["p23"]=[26];
+ph["p22"]=[24];
+ph["p24"]=[27];
 var keywordsInfo = {
     keywords: keywords,
     ph: ph
